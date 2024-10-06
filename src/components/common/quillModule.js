f6e7e9feaf4073f6058f51ce9e7577532dce41ac
Arn3092 <arnold.bkbck@gmail.com>
2024-10-06 20:23:47 +0530

    corrected form submission on enter

--- src/components/common/quillModule.js
+++ src/components/common/quillModule.js
@@ -10,4 +10,18 @@ export const modules = {
       // [{ 'align': [] }],
       ['clean'] // remove formatting button
     ],
+
+    keyboard: {
+      bindings: {
+        enter: {
+          key: 13, // Enter key
+          handler: function(range, context) {
+            // Handle Enter key press inside Quill
+            // console.log("Enter pressed inside Quill editor");
+            // Allow the default Quill behavior (e.g., inserting new lines)
+            return true;
+          }
+        }
+      }
+    }
 };
